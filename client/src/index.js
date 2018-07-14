783c0b90de571a723edaf2052852b3c0c232b012
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2018-07-14 22:25:47 +0300

    Added Apollo wrapper code

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/tachyons.min.css';
-import './styles/index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+// 1
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// 2
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+// 3
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+
+// 4
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+    , document.getElementById('root')
+)
+registerServiceWorker()
