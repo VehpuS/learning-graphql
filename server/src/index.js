754b7c51ddd3b1dafb6d7c04471894c0b2bbf5d0
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2018-07-16 00:29:38 +0300

    Added CRUD operations for posts

--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,19 @@
 import { GraphQLServer } from 'graphql-yoga'
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
-
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
 }]
+let idCount = links.length
 
+const findLink = (id) => links.find(link => link.id === id)
 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         feed: () => links,
+        link: (root, { id }) => findLink(id)
     },
     // because the implementation of the Link resolvers is trivial, you can actually omit them and the server will work in the same way as it did before
     // Link: {
@@ -31,10 +21,45 @@ const resolvers = {
     //     description: (root) => root.description,
     //     url: (root) => root.url,
     // }
+    Mutation: {
+        post: (root, { description, url}) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description,
+                url,
+            }
+            links.push(link)
+            return link
+        },
+        updateLink: (root, {id, url, description}) => {
+            links = links.map((link) =>
+                (link.id === id) ? 
+                    {
+                        ...link,
+                        url: url || link.url,
+                        description: description || link.description
+                    } :
+                    link)
+            return findLink(id)
+        },
+        deleteLink: (root, {id}) => {
+            const deletedLink = findLink(id)
+            links = links.filter(link =>
+                (link.id !== id))
+            return deletedLink
+        },
+    },
+
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    /*
+     * One convenient thing about the constructor of the GraphQLServer is that
+     * typeDefs can be provided either directly as a string (as you previously
+     * did) or by referencing a file that contains your schema definition
+     * (this is what you’re doing now).
+     */
+    typeDefs: './src/schema.graphql',
     resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
