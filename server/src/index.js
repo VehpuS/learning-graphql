bbf1cfb35c9884b42700d558a6fe9c0e8cb70243
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2018-07-15 09:01:38 +0300

    Added another resolver for in memory links

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,38 @@
 import { GraphQLServer } from 'graphql-yoga'
 
-// 1
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
 
-// 2
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+
+
 const resolvers = {
     Query: {
-        info: () => `This is the API of a GraphQL tutorial`
-    }
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+    // because the implementation of the Link resolvers is trivial, you can actually omit them and the server will work in the same way as it did before
+    // Link: {
+    //     id: (root) => root.id,
+    //     description: (root) => root.description,
+    //     url: (root) => root.url,
+    // }
 }
 
-// 3
 const server = new GraphQLServer({
     typeDefs,
     resolvers,
