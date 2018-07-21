04ce9eb5d8345ba037ec1865ea285e7e8ace4e61
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2018-07-22 01:51:11 +0300

    Added filters, pagination and ordering

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,6 +1,19 @@
-const feed = (parent, args, context, info) =>
-    context.db.query.links({}, info)
+const feed = (parent, args, context, info) => {
+    const where = 
+        args.filter ?
+        {
+            OR: [
+                { url_contains: args.filter },
+                { description_contains: args.filter },
+            ],
+        } : {}
 
+    const { skip, first, orderBy} = args
+    const queryArgs = { where, skip, first, orderBy }
+    return context.db.query.links(queryArgs, info)
+}
+
+    
 const Query = {
     feed
 }
