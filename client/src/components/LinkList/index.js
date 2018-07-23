71ac83bfa5b9daaff67d7c56b946a1aacef29999
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2018-07-24 00:37:11 +0300

    Connected client to GQL server

--- client/src/components/LinkList/index.js
+++ client/src/components/LinkList/index.js
@@ -1,16 +1,40 @@
-import React, { Component } from 'react'
-import Link from '../Link'
-import linksToRender from './mockLinks'
+import React from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
-class LinkList extends Component {
-    render() {
-        return (
-            <div>
-                {linksToRender.map(
-                    link => <Link key={link.id} link={link} />)}
-            </div>
-        )
+
+import Link from '../Link'
+
+const LinkList = ({feedQuery}) => {
+    if (feedQuery && feedQuery.loading) {
+        return <div>Loading</div>
     }
+
+    if (feedQuery && feedQuery.error) {
+        return <div>Error</div>
+    }
+
+    const linksToRender = feedQuery.feed.links
+
+    return (
+        <div>
+            {linksToRender.map(link =>
+                <Link key={link.id} link={link} />)}
+        </div>
+    )
 }
 
-export default LinkList
+const FEED_QUERY = gql`
+  query FeedQuery {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
+
+export default graphql(FEED_QUERY, { name: 'feedQuery' })(LinkList)
--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
-import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
-// 1
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
-// 2
+import './styles/index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+
+
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
-// 3
 const client = new ApolloClient({
     link: httpLink,
     cache: new InMemoryCache()
 })
 
-// 4
 ReactDOM.render(
     <ApolloProvider client={client}>
         <App />
