c623e82222605df6213fab005576bcd6fabf595f
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2018-07-25 01:14:48 +0300

    Added the ability to create a link with a static token in code

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
+import { ApolloLink } from 'apollo-client-preset'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
@@ -12,8 +13,22 @@ import registerServiceWorker from './registerServiceWorker'
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+    // const token = localStorage.getItem(AUTH_TOKEN)
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjamp2ZG51bzUwMDNqMDc1N3lzcnozOW9qIiwiaWF0IjoxNTMyMTc1MzY1fQ.OeqIQt8metmDLotblsvs-0h5lxsmsd6XVwDpVq_kMOY"
+    const authorizationHeader = token ? `Bearer ${token}` : null
+    operation.setContext({
+        headers: {
+            authorization: authorizationHeader
+        }
+    })
+    return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
+
 const client = new ApolloClient({
-    link: httpLink,
+    link: httpLinkWithAuthToken,
     cache: new InMemoryCache()
 })
 
